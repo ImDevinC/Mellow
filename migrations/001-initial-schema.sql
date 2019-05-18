c1afc919e6c387b644e45af429a83ae52cc9e11b
Devin Collins <me@imdevinc.com>
2019-05-17 21:08:36 -0600

    Make migration optional due to initial configurations

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -1,10 +1,10 @@
 -- Up
-CREATE TABLE general (id integer primary key asc, username text, password text);
-CREATE TABLE bot (id integer primary key asc, token text, ownerid text, commandprefix text, deletecommandmessages text, unknowncommandresponse text);
-CREATE TABLE ombi (id integer primary key asc, host text, port text, apikey text, requesttv text, requestmovie text);
-CREATE TABLE tautulli (id integer primary key asc, host text, port text, apikey text);
-CREATE TABLE sonarr (id integer primary key asc, host text, port text, apikey text);
-CREATE TABLE radarr (id integer primary key asc, host text, port text, apikey text);
+CREATE TABLE IF NOT EXISTS general (id integer primary key asc, username text, password text);
+CREATE TABLE IF NOT EXISTS bot (id integer primary key asc, token text, ownerid text, commandprefix text, deletecommandmessages text, unknowncommandresponse text);
+CREATE TABLE IF NOT EXISTS ombi (id integer primary key asc, host text, port text, apikey text, requesttv text, requestmovie text);
+CREATE TABLE IF NOT EXISTS tautulli (id integer primary key asc, host text, port text, apikey text);
+CREATE TABLE IF NOT EXISTS sonarr (id integer primary key asc, host text, port text, apikey text);
+CREATE TABLE IF NOT EXISTS radarr (id integer primary key asc, host text, port text, apikey text);
 
 -- Down
 DROP TABLE general;
